bcb4d044e0fb74b69d332af288c65e9edebdf12f
TheN4meless0ne <159443781+TheN4meless0ne@users.noreply.github.com>
2024-02-09 09:16:08 +0100

    Add files via upload

--- Python RPG/main.py
+++ Python RPG/main.py
@@ -1,6 +1,10 @@
 from character import Hero, Enemy
 from weapon import rusty_sword
 
+# ------------ future references ------------
+# hero.equip(rusty_sword)
+# hero.drop()
+
 hero = Hero(name="Hero", health=100)
 enemy = Enemy(name="Enemy", health=80, weapon=rusty_sword)
 
@@ -8,7 +12,8 @@ while True:
   hero.attack(enemy)
   enemy.attack(hero)
 
-  print(f"Health of {hero.name}: {hero.health}")
-  print(f"Health of {enemy.name}: {enemy.health}")
+  hero.health_bar.draw()
+  enemy.health_bar.draw()
+
 
   input()
