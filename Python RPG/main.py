dcd35caf508f2ac99e5f3e75030a140349cea57d
TheN4meless0ne <159443781+TheN4meless0ne@users.noreply.github.com>
2024-02-09 08:15:31 +0100

    Add files via upload

--- Python RPG/main.py
+++ Python RPG/main.py
@@ -1,60 +1,14 @@
-# Imports
-import json
-import random
+from character import Hero, Enemy
+from weapon import rusty_sword
 
+hero = Hero(name="Hero", health=100)
+enemy = Enemy(name="Enemy", health=80, weapon=rusty_sword)
 
-# Player stats
-class Player:
-  def __init__(self, name):
-    self.name = playerName
-    self.health = 100
-    self.attack = 10
-    self.crit_attack = self.attack * 1.5
-    self.crit_chance = 0.15
-    self.defense = 5
-    self.coins = 0
-  def is_alive(self):
-    return self.health > 0
-  def take_damage(self, damage):
-    self.health -= damage
-  def attack_enemy(self, enemy):
-    damage = self.attack
-    enemy.take_damage(damage)
-    print(f"{self.name} attacks {enemy.name} for {damage} damage.")
-  def __str__(self):
-    return f"{self.name} - Health: {self.health}, Coins: {self.coins}"
+while True:
+  hero.attack(enemy)
+  enemy.attack(hero)
 
-class Enemy:
-  def __init__(self, name, health, attack):
-    self.name = name
-    self.health = health
-    self.attack = attack
-  def is_alive(self):
-    return self.health > 0
-  def take_damage(self, damage):
-    self.health -= damage
-  def attack_player(self, player):
-    damage = self.attack
-    player.take_damage(damage)
-    print(f"{self.name} attacks {player.name} for {damage} damage.")
+  print(f"Health of {hero.name}: {hero.health}")
+  print(f"Health of {enemy.name}: {enemy.health}")
 
-# Main game loop
-def main():
-  playerName = input("What be the name of your champion? ")
-  print("Welcome to the dungeon,", playerName)
-  player = Player(playerName)
-
-  # List of enemies you can encounter
-  enemies = [
-    Enemy("Weak inmate", 60, 7),
-  ]
-
-  # Game starts
-  print("You wake up in a dark room.")
-  
-
-
-
-
-if __name__ == "__main__":
-  main()
+  input()
---
+++ Python RPG/weapon.py
@@ -0,0 +1,17 @@
+class weapon:
+  def __init__(self, name: str, weapon_type: str,damage: int, value: int) -> None:
+    self.name = name
+    self.weapon_type = weapon_type
+    self.damage = damage
+    self.value = value
+
+
+fists = weapon(name="Fists",
+              weapon_type="body",
+              damage= 2,
+              value= 0)
+
+rusty_sword = weapon(name="Rusty Sword",
+                    weapon_type="sword",
+                    damage= 10,
+                    value= 2)
